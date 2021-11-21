5c6123901f5d184a16e3321dda6196740c8a7010
senTristanC <wrecker331@gmail.com>
2021-11-20 18:46:33 -0800

    Worked on the enemy data dictionary and how that is going to be formatted. Lots of work on player. Key detection is working, so is player movement physics.

--- my-electron-app/scripts/main.js
+++ my-electron-app/scripts/main.js
@@ -4,7 +4,7 @@ let ctx = c.getContext("2d");
 
 let previousTime = 0.0;
 
-// dt = 7
+// dt = ~7
 
 const loop = time => {
     // Compute the delta-time against the previous time
@@ -27,8 +27,76 @@ window.requestAnimationFrame(time => {
     window.requestAnimationFrame(loop);
 });
 
-// Declare camera object
 
+// Gameplay Constants
+let friction = 0.9
+
+function update(dt) {
+
+    calculatePlayerMovement(dt)
+}
+
+function render() {
+    // Begin render
+    ctx.clearRect(0, 0, c.width, c.height);
+    ctx.beginPath(); // Used for drawing non-images
+
+    // Draw temp square as player
+
+    ctx.fillStyle = "#FFFFFF";
+    ctx.fillRect(player.xPosition, player.yPosition, 20, 20)
+
+
+    // Draws X and Y for camera
+    ctx.fillStyle = "#FF0000";
+    ctx.font = `20px Verdana`;
+    ctx.fillText(`X:${enemyDataDictionary[0].lifespan} Y:${camera.yPosition}`, 0, 20);
+    ctx.fillStyle = "#000000";
+}
+
+// Key Handler
+
+document.addEventListener('keydown', keyDownHandler, false);
+document.addEventListener('keyup', keyUpHandler, false);
+
+let rightPressed = false;
+let leftPressed = false;
+let upPressed = false;
+let downPressed = false;
+
+// Detects key down
+function keyDownHandler(event) {
+    if(event.keyCode === 39) {
+        rightPressed = true;
+    }
+    else if(event.keyCode === 37) {
+        leftPressed = true;
+    }
+    if(event.keyCode === 40) {
+        downPressed = true;
+    }
+    else if(event.keyCode === 38) {
+        upPressed = true;
+    }
+}
+
+// Detects key up
+function keyUpHandler(event) {
+    if(event.keyCode === 39) {
+        rightPressed = false;
+    }
+    else if(event.keyCode === 37) {
+        leftPressed = false;
+    }
+    if(event.keyCode === 40) {
+        downPressed = false;
+    }
+    else if(event.keyCode === 38) {
+        upPressed = false;
+    }
+}
+
+// Declare camera object
 const camera = {
     xPosition: 0,
     yPosition: 0,
@@ -53,25 +121,38 @@ const player = {
     health: 100,
     assetsLocation: '/assets/playerAssets',
     weaponSlot: {
-        1: "Pistol",
-        2: "Rifle",
+        primary: "Rifle",
+        secondary: "Pistol",
+        melee: "Knife",
     },
-
 };
 
-function update(dt) {
+// Enemy Dictionary
+const enemyDataDictionary = [
+    {
+        type: 'Crawler',
+        maxHealth: 100,
+        health: 70,
+        lifespan: 0,
+        assetsLocation: '/assets/enemyAssets/enemy_0',
+    },
+];
 
-}
+function calculatePlayerMovement(dt) {
 
-function render() {
-    ctx.beginPath();
-    ctx.clearRect(0, 0, c.width, c.height);
-    //ctx.globalAlpha = 1 ;
-    //ctx.fillRect(0,0, c.width, c.height);
-    ctx.globalAlpha = 1;
-    ctx.fillStyle = "#FF0000";
-    ctx.font = `20px Verdana`;
-    ctx.fillText(`X:${camera.xPosition} Y:${camera.yPosition}`, 0, 20);
-    ctx.fillStyle = "#000000";
+    // Player movement
+    if (upPressed === true) { player.yAcceleration = -1 }
+    if (downPressed === true) { player.yAcceleration = 1 }
+    if (leftPressed === true) { player.xAcceleration = -1 }
+    if (rightPressed === true) { player.xAcceleration = 1 }
+
+    player.xVelocity = player.xAcceleration * dt * friction
+    player.yVelocity = player.yAcceleration * dt * friction
+
+    player.xAcceleration = 0
+    player.yAcceleration = 0
+
+    player.xPosition += player.xVelocity
+    player.yPosition += player.yVelocity
 
 }
